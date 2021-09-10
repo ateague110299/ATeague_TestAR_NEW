19b0fdfa5519abd0e592c25c7cbef39bf688bbd1
MSI\andre <andrewt100@outlook.com>
2021-09-09 18:53:28 -0700

    Banan

---
+++ Assets/Scripts/ARCast.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARCast : MonoBehaviour
+{
+    public Camera ARCamera;
+    public GameObject objectRef;
+    public List<GameObject> Planes;
+    public ARPlaneManager ARPM;
+    public static ARCast ARRC;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ARRC = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began)
+            {
+                Ray ray = ARCamera.ScreenPointToRay(touch.position);
+                RaycastHit hitObject;
+                if (Physics.Raycast(ray, out hitObject))
+                {
+                    objectRef.transform.position = hitObject.point;
+                }
+            }
+        }
+    }
+}
