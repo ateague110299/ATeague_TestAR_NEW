0ad3e3f0a553bca7dd27e5d8810984d7eff3df83
MSI\andre <andrewt100@outlook.com>
2021-10-19 18:39:33 -0500

    Yeet

--- Assets/Scripts/CollisionLogic.cs
+++ Assets/Scripts/CollisionLogic.cs
@@ -5,29 +5,27 @@ using UnityEngine.XR.ARFoundation;
 
 public class CollisionLogic : MonoBehaviour
 {
-    public GameObject door;
     public GameObject lookingIN;
     public GameObject lookingOUT;
-    public GameObject Back;
-    public GameObject DefaultLayerChildren;
-    // Start is called before the first frame update
+    public List<Transform> objectsToIgnoreCollisionLogic;
+    public GameObject ObjectHolder;
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Door")
+        if (other.gameObject.tag == "Door")
         {
-            foreach(Transform obj in DefaultLayerChildren.transform)
-            {
-                if (gameObject.transform.position.z > door.transform.position.z)
-                {
-                    obj.gameObject.layer = 0;
-                    lookingIN.gameObject.SetActive(false);
-                    lookingOUT.gameObject.SetActive(true);
-                }
-                else
-                {
-                    obj.gameObject.layer = 7;
-                    lookingIN.gameObject.SetActive(true);
-                    lookingOUT.gameObject.SetActive(false);
+            Transform[] allChildren = ObjectHolder.GetComponentsInChildren<Transform>();
+            var direction = other.transform.position - transform.position;
+            foreach (var obj in allChildren){
+                if (!objectsToIgnoreCollisionLogic.Contains(obj)){
+                    if (Vector3.Dot(other.transform.forward, direction) < 0){
+                        obj.gameObject.layer = 8;
+                        lookingIN.gameObject.SetActive(false);
+                        lookingOUT.gameObject.SetActive(true);
+                    } else {
+                        obj.gameObject.layer = 7;
+                        lookingIN.gameObject.SetActive(true);
+                        lookingOUT.gameObject.SetActive(false);
+                    }
                 }
             }
         }
