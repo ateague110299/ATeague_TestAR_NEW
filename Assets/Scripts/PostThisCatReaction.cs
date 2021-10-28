c2495f8f08c7c210dff7b0c1ea13d64baad51a31
MSI\andre <andrewt100@outlook.com>
2021-10-27 20:51:02 -0500

    Added blank script for reaction to interacting with the scene

---
+++ Assets/Scripts/PostThisCatReaction.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostThisCatReaction : MonoBehaviour
+{
+    public GameObject yesButton;
+    public GameObject noButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
